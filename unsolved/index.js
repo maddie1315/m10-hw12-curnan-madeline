f0c1ffed040efbfae70f4dfe9bb59aee167f1cbb
madeline curnan <madelinecurnan@ufl.edu>
2021-12-04 14:58:20 -0500

    Add mySweetRide variable

--- unsolved/index.js
+++ unsolved/index.js
@@ -1,8 +1,8 @@
 class Car {
-    constructor(make, model) {
+    constructor(make, model, year) {
         this.make = make
         this.model = model
-        this.year = 0000
+        this.year = year
     }
 
     honk() {
@@ -15,3 +15,7 @@ class Car {
         }, 3000)
     }
 }
+
+var mySweetRide = new Car('Pontiac', 'Fiero', 1988)
+mySweetRide.honk()
+mySweetRide.performMaintenance()
