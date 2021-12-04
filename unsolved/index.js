584241deffd95558a7df6c96d984747a45c6bfcc
madeline curnan <madelinecurnan@ufl.edu>
2021-12-04 14:51:48 -0500

    Add car class

--- unsolved/index.js
+++ unsolved/index.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE
+class Car {
+    constructor(make, model) {
+        this.make = make
+        this.model = model
+        this.year = 0000
+    }
+
+    honk() {
+        console.log('BEEP BEEP!')
+    }
+
+    performMaintenance() {
+        setTimeout(function() {
+            console.log('maintenance complete')
+        }, 3000)
+    }
+}
